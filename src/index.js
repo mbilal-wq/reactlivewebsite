2b9ea4be4a835c55759bd90f7d6694d8e4771f15
muhammad bilal <mbilaldev226@gmail.com>
2024-07-02 18:51:29 +0500

    updated

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 ReactDOM.render(
   <>
-    <BrowserRouter>
+    <BrowserRouter basename="/reactlivewebsite">
       <App />
     </BrowserRouter>
   </>,
